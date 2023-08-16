5f867299d6d0e47657fdac801ae26e47448d13bd
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-08-16 21:13:37 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,12 @@ from src.parameters import HEADERS
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import time
-
-
+import pandas as pd
+import os
+import json
+import re
+import csv
+ 
 #генератор случайных чисел
 generator = np.random.default_rng()
 
@@ -25,7 +29,6 @@ def translateEN(text):
     return(result.text)
 
 def meke_prompt(cat_words,item,name,sostav,rangeDescription,keyWords):
-    print('______________________2')
     if sostav!='':
         return f"Сделай описание товара {item}, который называется {name} на маркетплейсе, с составом: {sostav}. Товар находиться в категориях : {cat_words}. Используй ключевые слова : {keyWords}. Также дай продуктовое обещание. Максимальная длина {rangeDescription} слов"
     return f"Сделай описание товара {item} на маркетплейсе, используя все слова из списка : {cat_words}. "
@@ -63,7 +66,9 @@ def get_top_words_browser(word_):
     idx1=src.find("]]")
     data=src[idx0+2:idx1]
     data=data.replace('"','')
+    
     data=data.split('],[')
+    
     word_dict={}
     for wordAN in data:
         word, _, _, count, _ = wordAN.split(',')
@@ -115,14 +120,168 @@ def get_requests_Internet(item,count=5):
     return req
 
 
+def get_category():
+    url = 'https://catalog.wb.ru/catalog/shealth2/catalog?appType=1&curr=rub&dest=-1257786&regions=80,38,83,4,64,33,68,70,30,40,86,75,69,22,1,31,66,110,48,71,114&spp=0&sort=null&cat=10327&uclusters=8'
+    response = requests.get(url=url)
+    return response.json()
 
+def prepare_items(response, limit=30):
+    products = []
+    products_raw = response.get('data', {}).get('products', None)
+    if products_raw is not None and len(products_raw) > 0:
+        for product in products_raw[:limit]:
+            products.append({
+                'id': product.get('id', None),
+                'product_link': f'https://www.wildberries.ru/catalog/{product.get("id")}/detail.aspx'
+            })
+    return products
 
+def save_urls_to_file(products, filename):
+    with open(filename, 'w') as f:
+        for product in products:
+            f.write(product['product_link'] + '\n')
 
+def get_product_data(url):
+    jsonData = None
+    article = int((re.findall(r'\d+', url))[0])
 
+    _short_id = article // 100000
+    if 0 <= _short_id < 144:
+        basket = '01'
+    elif 144 <= _short_id < 288:
+        basket = '02'
+    elif 288 <= _short_id < 432:
+        basket = '03'
+    elif 432 <= _short_id < 720:
+        basket = '04'
+    elif 720 <= _short_id < 1008:
+        basket = '05'
+    elif 1008 <= _short_id < 1062:
+        basket = '06'
+    elif 1062 <= _short_id < 1116:
+        basket = '07'
+    elif 1116 <= _short_id < 1170:
+        basket = '08'
+    elif 1170 <= _short_id < 1314:
+        basket = '09'
+    elif 1314 <= _short_id < 1602:
+        basket = '10'
+    elif 1602 <= _short_id < 1656:
+        basket = '11'
+    elif 1656 <= _short_id < 1920:
+        basket = '12'
+    else:
+        basket = '13'
+    rating = 0
+    feedbacks = 0
+    url = f"https://basket-{basket}.wb.ru/vol{_short_id}/part{article // 1000}/{article}/info/ru/card.json"
+    res = requests.get(url=url)
+    if res.status_code == 200:
+        jsonData = json.loads(res.text)
 
+        imt_id = jsonData["imt_id"]
+        try:
+            description = jsonData["description"]
+        except:
+            description = "No description available"
+        try:
+            brand = jsonData["selling"]["brand_name"]
+        except:
+            brand = "NULL"
+        supplier_id = jsonData["selling"]["supplier_id"]
+        photo_count = jsonData["media"]["photo_count"]
+    url = f"https://feedbacks1.wb.ru/feedbacks/v1/{imt_id}"
+    res = requests.get(url=url)
+    if res.status_code == 200:
+        jsonData = json.loads(res.text)
+    url = f"https://card.wb.ru/cards/detail?appType=1&curr=rub&dest=-1257786&regions=80,38,83,4,64,33,68,70,30,40,86,75,69,22,1,31,66,110,48,71,114&spp=0&nm={article}"
+    res = requests.get(url=url)
+    if res.status_code == 200:
+        jsonData = json.loads(res.text)
+        rating = jsonData['data']['products'][0]["reviewRating"]
+        sale = jsonData['data']['products'][0]["sale"]
+        feedbacks = jsonData['data']['products'][0]["feedbacks"]
+        name = jsonData['data']['products'][0]["name"]
+        qty = jsonData['data']['products'][0]['sizes'][0]['stocks'][0]["qty"]
+        price = jsonData['data']['products'][0]["salePriceU"]/100
+    url = f"https://product-order-qnt.wildberries.ru/by-nm/?nm={article}"
+    res = requests.get(url=url)
+    if res.status_code == 200:
+        jsonData = json.loads(res.text)
+        buysNum = jsonData[0]["qnt"]
+    url = f"https://basket-{basket}.wb.ru/vol{_short_id}/part{article // 1000}/{article}/images/big/1.jpg"
+    res = requests.get(url=url)
+    if res.status_code == 200:
+        image_link = "".join([
+            f"https://basket-{basket}.wb.ru/vol{_short_id}/part{article // 1000}/{article}/images/big/1.jpg "])
+    return [{
+        "imt_id": imt_id,
+        "feedbacks": feedbacks,
+        "brand": brand,
+        "name": name.replace(";", ","),
+        "stars": rating,
+        "qty": qty,
+        "sale": sale,
+        "price": price,
+        "buysNum": buysNum,
+        "image_link": image_link,
+        "description": re.sub(r'\n', '', description.replace(";", ","))
+    }]
 
+def parse_top_WB(limit=30):
+    response = get_category()
+    products = prepare_items(response, limit=limit)
 
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(project_dir, 'urls.txt')
 
+    save_urls_to_file(products, file_path)
 
+    data = []
+    time_list = []
+    strN = 0
 
+    with open(file_path) as rf:
+        with open("./storage/data.csv", 'w', encoding="cp1251", errors='replace', newline='') as csvfile:
+            url = rf.readline()
+            spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|')
 
+            column_names = [
+                "цена",
+                "название",
+                "скидка",
+                "рейтинг float",
+                "в наличии",
+                "Кол-во отзывов",
+                "описание",
+                "Куплено за год",
+                "изображения",
+                "Отзывы символы"
+            ]
+            spamwriter.writerow(column_names)
+
+            while url:
+                timer_begin = time.time()
+                dataGet = get_product_data(url)
+                spamwriter.writerow([
+                    dataGet[0]["price"],
+                    dataGet[0]["name"],
+                    dataGet[0]["sale"],
+                    dataGet[0]["stars"],
+                    dataGet[0]["qty"],
+                    dataGet[0]["feedbacks"],
+                    dataGet[0]["description"],
+                    dataGet[0]["buysNum"],
+                    dataGet[0]["image_link"],
+                    len(dataGet[0]["description"])
+                ])
+                data = data + [dataGet]
+                timer_end = time.time()
+                timer_secs = timer_end - timer_begin
+                time_list.append(timer_secs)
+                strN += 1
+                url = rf.readline()
+
+    jsonStr = json.dumps(data, sort_keys=False, ensure_ascii=False, separators=(',', ': '))
+    with open("./storage/data.json", "a", encoding='utf-8') as f:
+        f.write(jsonStr)
