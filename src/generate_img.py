ec5e192da6f6d9b3a5e92408d5a349a7f364fcbd
Net1dei <130304004+Net1dei@users.noreply.github.com>
2023-08-16 17:18:01 +0300

    Update generate_img.py

--- src/generate_img.py
+++ src/generate_img.py
@@ -27,14 +27,14 @@ async def gen_imag():
             await page.locator("//div[@id='fid-result-img']//a[1]").click()
         download = await download_info.value
         print(await download.path())
-        await download.save_as("c:\\Users\\Admin\\Desktop\\ababab\\1.jpg")  
+        await download.save_as("./storage/1.jpg")  
     
 async def downloadurl(prompt):
     async with async_playwright() as p: 
         browser = await p.firefox.launch(headless=False)
         page = await browser.new_page()
         await page.goto("https://imgcreator.zmo.ai/tools/background-changer")
-        await page.set_input_files("input[type='file']", '1.jpg')
+        await page.set_input_files("input[type='file']", './storage/1.jpg')
         await page.get_by_role("textbox", name="Email*").fill('kekw171@outlook.com')
         await page.get_by_role("textbox", name="Password*").fill('Trof9911')
         time.sleep(2)
@@ -54,12 +54,12 @@ async def downloadurl(prompt):
 
         image_url = await result.get_attribute("src") 
         url = image_url.replace(" ", "%20")
-        urllib.request.urlretrieve(url, 'c:\\Users\\Admin\\Desktop\\ababab\\2.webp')  
+        urllib.request.urlretrieve(url, './storage/2.webp')  
 
 def img_and_border():
-    backgroung_img = Image.open("2.webp")
+    backgroung_img = Image.open("./storage/2.webp")
 
-    foreground_img=Image.open('ramka.png')
+    foreground_img=Image.open('./storage/ramka.png')
     crop_width = 600
     crop_height = 800
     img_width, img_height = backgroung_img.size
@@ -69,16 +69,16 @@ def img_and_border():
                          (img_height + crop_height) // 2))
     foreground_img = foreground_img.resize((lk.width, lk.height))
     lk.paste(foreground_img, (0,0), foreground_img)
-    lk.save('3.png')
+    lk.save('./storage/3.png')
 
 def srift(txt):
-    img = Image.open('3.png')
+    img = Image.open('./storage/3.png')
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype('arial.ttf', 20, encoding='UTF-8')
     w, h = draw.textsize(txt)
     a = 600
     draw.text(((a-w)/2, 10), txt, (255, 255, 255), font=font)
-    img.save('3.png')
+    img.save('./storage/3.png')
 
 def gen_img(imgB64, prompt, txt):
     create_txt(imgB64=imgB64)
