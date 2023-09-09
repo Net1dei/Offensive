20add168f93edcf95438e16b35ad46641903c14b
Net1dei <130304004+Net1dei@users.noreply.github.com>
2023-09-09 07:40:50 +0300

    Update generate_text.py

--- src/generate_text.py
+++ src/generate_text.py
@@ -31,37 +31,7 @@ def generate_text_gpt(prompt):
 
 
 def generate_text_llama2(prompt):
-    try:
-        options.headless = True
-        driver = webdriver.Edge(options)
-        driver.get('https://huggingface.co/chat/')
-        time.sleep(4)
-        elText = driver.find_element(By.TAG_NAME, "textarea")
-        elText.send_keys(prompt)
-        ActionChains(driver) \
-            .key_down(Keys.ENTER) \
-            .key_up(Keys.ENTER) \
-            .perform()
-        time.sleep(2)
-        elTextPast = ""
-        elText = driver.find_element(
-            By.XPATH, "//div[contains(@class,'relative min-h-[calc(2rem+theme(spacing[3.5])*2)]')]")
-        while elText.text != elTextPast:
-            elTextPast = elText.text
-            elText = driver.find_element(
-                By.XPATH, "//div[contains(@class,'relative min-h-[calc(2rem+theme(spacing[3.5])*2)]')]")
-            time.sleep(1)
-        if elText.text == None:
-            return ("wrong reqest")
-        text = elText.text
-
-        return (translateRU(text))
-
-    except Exception as ex:
-        print(ex)
-    finally:
-        driver.close()
-        driver.quit()
+    return('aboba')
 
 
 def generate_text(prompt, chat='gpt'):
